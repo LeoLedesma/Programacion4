f7ce070ec4c9c45131d331e9e3fcedf9eb619286
LeoLedesma <leofledesma@gmail.com>
2023-03-30 22:12:41 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primera-app-angular';
+  titulo = 'Clase 1';
+
+  //#region Clase
+
+  variableInput = ''
+  //No hay int no hay float no hay number
+  numero1 : number = 0;
+  numero2 : number = 0;
+  resultado : number = 0;
+  imagen: string = '../assets/Screenshot_1.png'
+//#endregion
+
+  edadUno:number = 0;
+  edadDos:number = 0;
+  promedio:number = 0;
+  
+
+  Sumar(){
+    this.resultado = this.numero1 + Number(this.numero2);
+  }
+
+  Promedio(){
+    this.promedio = (this.edadUno+this.edadDos)/2;
+  }
+
+  LimpiarForm(){
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+  }
+
+
+  cambiarTitulo (tituloNuevo: any){
+    this.titulo = tituloNuevo;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule,
+    FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
