eabbff5a3cbbf759f8ea05314cd64fb636cc2a1e
LeoLedesma <leofledesma@gmail.com>
2023-04-24 00:33:43 -0300

    Fix ejercicio 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { Clase2RoutingModule } from './Clase2/clase2-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
+import { Ejercicio1Component } from './Clase1/ejercicio1/ejercicio1.component';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
-    InicioComponent
+    InicioComponent,
+    Ejercicio1Component
   ],
   imports: [
     BrowserModule,
